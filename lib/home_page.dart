53b5cdd07abf91397e21fdcd9f04b6c6de691259
Shahed <shahed_hridoy@outlook.com>
2023-03-19 01:47:23 +0600

    project run

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,13 +2,157 @@ import 'package:flutter/material.dart';
 import 'todo_list_view_model.dart';
 import 'todo.dart';
 
-
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
-
 }
 
-class _HomePageState extends State(HomePage){
+class _HomePageState extends State<HomePage> {
   final _viewModel = TodoListViewModel();
+
+  @override
+  void initState() {
+    super.initState();
+    _viewModel.fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CRUD with REST API (MVVM)'),
+      ),
+      body: StreamBuilder<List<Todo>>(
+        stream: _viewModel.todoListStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          List<Todo> todoList = snapshot.data!;
+          return ListView.builder(
+            itemCount: todoList.length,
+            itemBuilder: (context, index) {
+              Todo todo = todoList[index];
+              return ListTile(
+                title: Text(todo.title),
+                subtitle: Text(todo.completed ? 'Completed' : 'Not completed'),
+                trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    _viewModel.deleteData(todo);
+                  },
+                ),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      String title = todo.title;
+                      bool completed = todo.completed;
+
+                      return AlertDialog(
+                        title: Text('Update item'),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextField(
+                              onChanged: (value) {
+                                title = value;
+                              },
+                              decoration: InputDecoration(
+                                labelText: 'Title',
+                              ),
+                              controller:
+                                  TextEditingController(text: todo.title),
+                            ),
+                            CheckboxListTile(
+                              value: completed,
+                              onChanged: (value) {
+                                completed = value!;
+                              },
+                              title: Text('Completed'),
+                            ),
+                          ],
+                        ),
+                        actions: [
+                          TextButton(
+                            child: Text('Cancel'),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                          TextButton(
+                            child: Text('Save'),
+                            onPressed: () {
+                              _viewModel.updateData(todo, title, completed);
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              String title = '';
+              bool completed = false;
+
+              return AlertDialog(
+                title: Text('Add item'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextField(
+                      onChanged: (value) {
+                        title = value;
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Title',
+                      ),
+                    ),
+                    CheckboxListTile(
+                      value: completed,
+                      onChanged: (value) {
+                        completed = value!;
+                      },
+                      title: Text('Completed'),
+                    ),
+                  ],
+                ),
+                actions: [
+                  TextButton(
+                    child: Text('Cancel'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  TextButton(
+                    child: Text('Add'),
+                    onPressed: () {
+                      _viewModel.addData(title, completed);
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
 }
--- lib/todo_list_view_model.dart
+++ lib/todo_list_view_model.dart
@@ -3,6 +3,53 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'todo.dart';
 
+class TodoListViewModel {
+  final StreamController<List<Todo>> _todoListController =
+      StreamController<List<Todo>>.broadcast();
+  List<Todo> _todoList = [];
 
+  Stream<List<Todo>> get todoListStream => _todoListController.stream;
 
-class Todo
+  Future<void> fetchData() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(response.body);
+      _todoList = body.map((dynamic item) => Todo.fromJson(item)).toList();
+      _todoListController.add(_todoList);
+    } else {
+      throw Exception('Failed to load todo list');
+    }
+  }
+
+  void addData(String title, bool completed) {
+    final newTodo = Todo(
+      id: _todoList.length + 1,
+      title: title,
+      completed: completed,
+    );
+    _todoList.add(newTodo);
+    _todoListController.add(_todoList);
+  }
+
+  void updateData(Todo todo, String title, bool completed) {
+    final index = _todoList.indexOf(todo);
+    final updatedTodo = Todo(
+      id: todo.id,
+      title: title,
+      completed: completed,
+    );
+    _todoList[index] = updatedTodo;
+    _todoListController.add(_todoList);
+  }
+
+  void deleteData(Todo todo) {
+    _todoList.remove(todo);
+    _todoListController.add(_todoList);
+  }
+
+  void dispose() {
+    _todoListController.close();
+  }
+}
