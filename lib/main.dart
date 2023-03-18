7bb9208e11d1940b33ace88f62cfae56a65cbe6c
Shahed <shahed_hridoy@outlook.com>
2023-03-19 01:49:58 +0600

    debug mode banner remove

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
       title: 'CRUD with REST API (MVVM)',
     );
